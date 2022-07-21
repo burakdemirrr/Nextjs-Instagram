d7362ac7114606cb275e915835cf1ba805c34ba8
Burak <mburak.demir.059@gmail.com>
2022-07-21 21:46:44 +0300

    Sign In Page UI

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -6,8 +6,9 @@ export default function signin({ providers }) {
     <>
     <Header/>
       {Object.values(providers).map((provider) => (
-        <div key={provider.name}>
-          <button onClick={() => signIn(provider.id,{callbackUrl:"/"})}>
+        <div key={provider.name} className="flex items-center justify-center flex-col">
+          <img className="h-64 w-64 object-contain" src="http://localhost:3000/_next/image?url=https%3A%2F%2Flinks.papareact.com%2Focw&w=1920&q=75" alt="" />
+          <button className="border-2 p-3 bg-gray-400" onClick={() => signIn(provider.id,{callbackUrl:"/"})}>
             Sign in with {provider.name}
           </button>
         </div>
